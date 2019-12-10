3a9f9d9adf0c970fdad196f76c63dd5399f59a32
nbrownst <38437925+nbrownst@users.noreply.github.com>
2019-12-10 17:03:02 -0500

    Update Table1_biom.R
    Remove unnecessary lines (they were all commented out anyway)

--- Simulations/Simulations_run_other_methods/Table1_Biom.R
+++ Simulations/Simulations_run_other_methods/Table1_Biom.R
@@ -12,24 +12,8 @@ set.seed(12321)
 #options(error=traceback)
 #syntax is as follows: dosims(truebeta,mcar,misspec)
 
-#test
-#dosims(-0.5,0,0,nsim=2)
-
 
 #run with 250
 dosims(-0.5,0,0,nsim=250,nx=1000)
 dosims(-1.5,0,0,nsim=250,nx=1000)
 dosims(-3,0,0,nsim=250,nx=1000)
-
-######not needed
-##change sample size
-#set.seed(12321)
-#dosims(-0.5,0,0,nsim=250,nx=2000)
-#dosims(-1.5,0,0,nsim=250,nx=2000)
-#dosims(-3,0,0,nsim=250,nx=2000)
-
-#set.seed(12321)
-
-#dosims(-0.5,0,0,nsim=250,nx=5000)
-#dosims(-1.5,0,0,nsim=250,nx=5000)
-#dosims(-3,0,0,nsim=250,nx=5000)
